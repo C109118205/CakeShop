964e63e93fe4cb7b1d5b27fa85b56742c082cb41
C109118205 <C109118205@nkust.edu.tw>
2023-01-08 23:22:52 +0800

    update map

--- app/src/main/java/com/example/cakeshop/HomeFragment.java
+++ app/src/main/java/com/example/cakeshop/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.cakeshop;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -14,9 +17,15 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.cakeshop.databinding.FragmentHomeBinding;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
+    private static final String[] Location_PERMISSION = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
 
   private FragmentHomeBinding binding;
 
@@ -31,6 +40,32 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        try {
+            MapsInitializer.initialize(requireActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        binding.mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+
+
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    requestPermissions(new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                    }, 0);
+                } else {
+                    googleMap.setMyLocationEnabled(true);
+                }
+            }
+        });
+        binding.mapView.onCreate(savedInstanceState);
+        binding.mapView.onResume();
 
         return root;
     }
@@ -44,6 +79,42 @@ public class HomeFragment extends Fragment {
             }
         });
     }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        binding.mapView.onStart();
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        binding.mapView.onResume();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        binding.mapView.onPause();
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        binding.mapView.onStop();
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        binding.mapView.onDestroy();
+    }
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        binding.mapView.onLowMemory();
+    }
 
 }
